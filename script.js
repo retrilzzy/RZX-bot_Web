bc59ffa8bf2e9ab32f7ac60fec49b30d5b31d1b8
Razy <129707458+RetrilzzY@users.noreply.github.com>
2024-02-27 19:34:33 +0600

    Update API server

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchData() {
     try {
-        const response = await fetch('https://eae11d00-9972-4185-ab96-7f8b1c62bf93-00-v9i6v80gr6zl.sisko.replit.dev/api/stats');
+        const response = await fetch('http://node3.endelon-hosting.de:12501/api/stats');
         const data = await response.json();
 
         document.getElementById('guildsCount').innerText = data.guilds;
