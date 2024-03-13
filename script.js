598a5211bd6529b5bfed35e30646817126d72e46
Razy <129707458+RetrilzzY@users.noreply.github.com>
2024-03-13 12:36:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ async function fetchData() {
 
         document.getElementById('guildsCount').innerText = data.guilds;
         document.getElementById('usersCount').innerText = data.users;
+        document.getElementById('cmdUsage').innerText = data.cmd_usage;
     } catch (error) {
         console.error('Error fetching data:', error);
         displayError();
@@ -14,6 +15,7 @@ async function fetchData() {
 function displayError() {
     document.getElementById('guildsCount').innerText = '50+';
     document.getElementById('usersCount').innerText = '1400+';
+    document.getElementById('cmdUsage').innerText = 'API ☠️';
 }
 
 window.onload = fetchData;
