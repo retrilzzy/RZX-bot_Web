09aa5422f145a5320b1f5940fa6d5baf46cf7652
Razy <129707458+RetrilzzY@users.noreply.github.com>
2024-03-16 13:53:36 +0500

    Update api url

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 async function fetchData() {
     try {
-        const response = await fetch('https://rzx.nexcord.pro/api/stats');
+        const response = await fetch('https://rzx-bot-rs.nexcord.pro/api/stats');
         const data = await response.json();
 
         document.getElementById('guildsCount').innerText = data.guilds;
         document.getElementById('usersCount').innerText = data.users;
         document.getElementById('cmdUsage').innerText = data.cmd_usage;
+        
     } catch (error) {
         console.error('Error fetching data:', error);
         displayError();
@@ -15,7 +16,7 @@ async function fetchData() {
 function displayError() {
     document.getElementById('guildsCount').innerText = '50+';
     document.getElementById('usersCount').innerText = '1400+';
-    document.getElementById('cmdUsage').innerText = 'API ☠️';
+    document.getElementById('cmdUsage').innerText = 'API Down';
 }
 
 window.onload = fetchData;
